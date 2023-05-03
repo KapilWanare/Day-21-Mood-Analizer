d43a9015766a3e54d1d0e42bc37ec05a16892393
wanarekapil96@ <wanarekapil96@gmail.com>
2023-05-03 20:37:41 +0530

    first commit

---
+++ src/MoodAnalyzerException2.java
@@ -0,0 +1,12 @@
+public class MoodAnalyzerException2 extends Exception {
+
+    enum ExceptionType {
+        ENTERED_NULL , ENTERED_EMPTY
+    }
+    ExceptionType type;
+
+    public MoodAnalyzerException2(ExceptionType type ,String message) {
+        super(message);
+        this.type = type;
+    }
+}
